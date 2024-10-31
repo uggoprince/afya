be1b4309d778cd227b1984ee91357e673d390f6f
Ugo Anayo <Ugo.Anayo@Interswitchng.com>
2024-10-31 08:44:46 +0100

    add icons to sidebar links

--- app/components/Sidebar/SidebarItem.jsx
+++ app/components/Sidebar/SidebarItem.jsx
@@ -3,12 +3,13 @@ import dynamic from 'next/dynamic';
 
 // const Icon = ({src}) => dynamic(() => import(src), { ssr: false });
 
-const SidebarItem = ({ icon, label, link }) => {
+const SidebarItem = ({ icon, label, link, children }) => {
   // const Icon = dynamic(() => import(icon), { ssr: false });
     return (
       <Link href={link} to={link} className="sidebar-item" aria-label={label}>
         {/* <div className="icon">{icon}</div> */}
         {/* <Icon src={icon} width={24} height={24} fill="#e8eaed" /> */}
+        {children}
         <div className="label">{label}</div>
       </Link>
     );
--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -5,16 +5,24 @@ import WelcomeCard from "../components/WelcomeCard";
 import MetricCard from "../components/MetricCard";
 import ActivityGrowthChart from "../components/ActivityGrowthChart";
 import BMICalculator from "../components/BMICalculator";
-// import DashBoardIcon from "../../public/dashboard.svg";
+import DashBoardIcon from "../../public/dashboard.svg";
+import MedicationIcon from "../../public/medication.svg";
+import ProfileIcon from "../../public/profile.svg";
+import SettingsIcon from "../../public/settings.svg";
+import LogoutIcon from "../../public/logout.svg";
 
 export default function Home() {
     return (<div className="w-full flex flex-row overflow-hidden">
         <SideBar>
-            <SidebarItem label={'My Health'} link={'/dashboard'} icon={'../../public/dashboard.svg'} />
-            <SidebarItem label={'Medications'} link={'/dashboard'} />
-            <SidebarItem label={'Profile'} link={'/dashboard'} />
-            <SidebarItem label={'Settings'} link={'/dashboard'} />
-            <SidebarItem label={'Logout'} link={'/'} />
+            <SidebarItem label={'My Health'} link={'/dashboard'} icon={'../../public/dashboard.svg'}>
+                <DashBoardIcon fill={'#555555'} />
+            </SidebarItem>
+            <SidebarItem label={'Medications'} link={'/dashboard'}>
+                <MedicationIcon fill={'#555555'} />
+            </SidebarItem>
+            <SidebarItem label={'Profile'} link={'/dashboard'}><ProfileIcon fill={'#555555'} /></SidebarItem>
+            <SidebarItem label={'Settings'} link={'/dashboard'}><SettingsIcon fill={'#555555'}/></SidebarItem>
+            <SidebarItem label={'Logout'} link={'/'}><LogoutIcon fill={'#555555'}/></SidebarItem>
         </SideBar>
         <div className="main-content flex flex-col lg:flex-row flex-1 h-screen overflow-y-auto">
             <div className="flex flex-col flex-1 overflow-x-hidden w-full h-fit">
