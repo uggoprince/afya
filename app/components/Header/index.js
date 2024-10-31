7d5cf2f2f8158bebbd5f47c049edce1ee3f22c38
Ugo Anayo <Ugo.Anayo@Interswitchng.com>
2024-10-31 06:48:12 +0100

    create home page

---
+++ app/components/Header/index.js
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+import './style.css';
+
+const Header = (props) => {
+    return (
+        <header className="header">
+            <div className=' text-[28px] font-bold text-sky-700'>
+                AFYA
+            </div>
+            <div className='self-end my-auto flex flex-row gap-4 text-white'>
+                <Link href={'/'}
+                    className='py-2 px-3 rounded-md cursor-pointer border-sky-500 text-sky-950 hover:text-white hover:bg-sky-400'>Home</Link>
+                <Link href={'/dashboard'}
+                    className='py-2 px-3 rounded-md cursor-pointer border-sky-500 text-sky-950 hover:text-white hover:bg-sky-400'>DashBoard</Link>
+            </div>
+        </header>)
+};
+
+export default Header;
