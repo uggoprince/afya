8e667257298b5d8083d455a396d15c67cde2aae7
Ugo Anayo <Ugo.Anayo@Interswitchng.com>
2024-10-31 06:53:37 +0100

    fix button href

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -8,7 +8,7 @@ const Header = (props) => {
                 AFYA
             </div>
             <div className='self-end my-auto flex flex-row gap-4 text-white'>
-                <Link href={'/'}
+                <Link href={''}
                     className='py-2 px-3 rounded-md cursor-pointer border-sky-500 text-sky-950 hover:text-white hover:bg-sky-400'>Home</Link>
                 <Link href={'/dashboard'}
                     className='py-2 px-3 rounded-md cursor-pointer border-sky-500 text-sky-950 hover:text-white hover:bg-sky-400'>DashBoard</Link>
